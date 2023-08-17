e1209cf510aaab82b712b8dccd3e47942a7bbdb9
Silvio Traversaro <silvio@traversaro.it>
2023-08-17 17:52:29 +0200

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,3 +3,7 @@ gpmetis graphs/mdual.graph 10
 ndmetis graphs/mdual.graph 10
 gpmetis graphs/test.mgraph 10
 m2gmetis graphs/metis.mesh 10
+
+# Regression test for https://github.com/KarypisLab/METIS/issues/69
+${CXX} -lmetis metis_test.cpp -o metis_test
+./metis_test
