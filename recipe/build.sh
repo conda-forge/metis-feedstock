0695d4f5f921bab3e626495e36c4f9f02543c3f5
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-02-27 11:02:03 +0100

    Add GKlib build to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,30 @@
 
 set -ex
 
+pushd GKlib
+
+mkdir build
+mkdir static-libs
+
+pushd build
+
+cmake ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=../static-libs \
+      -DDEBUG=OFF \
+      -DOPENMP=set \
+      -DBUILD_SHARED_LIBS=OFF \
+      ..
+
+cmake --build . --config Release
+cmake --install .
+
+popd
+
+popd
+
 make config \
      shared=1 \
-     gklib_path=$PREFIX \
+     gklib_path=GKlib/static-libs \
      prefix=$PREFIX
 
 make
